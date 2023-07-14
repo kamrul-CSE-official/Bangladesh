2820e6baa976611366ecd76b4135dd45ab1f6dbf
MD.Kamrul Hasan <kamrulhasan635653@gmail.com>
2023-07-15 02:33:58 +0600

    update

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1">
+      <Text className="text-white">Bismillah...</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    Plugin: ["nativewind/babel"]
+    plugins: ["nativewind/babel"],
   };
 };
+    // plugins: ["nativewind/babel"],
